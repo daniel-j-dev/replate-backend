4dff10a032f13b37fa0e5c06d2b396433b8fe555
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-07-27 20:19:22 -0400

    Enabled cors for all routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const userRouter = require('./auth/userRouter.js');
 const pickupRequestsRouter = require('./auth/pickupRequestsRouter.js');
 
@@ -13,6 +14,7 @@ const port = process.env.PORT;
 
 //Middleware
 
+server.use(cors());
 server.use(express.json());
 server.use('/', userRouter);
 server.use('/pickup', pickupRequestsRouter);
