b04b09b0b23c745c70f36ecf650a13686e96944f
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-07-30 03:40:41 -0400

    Tests done

--- tests/allTests.spec.js
+++ tests/allTests.spec.js
@@ -1,7 +1,11 @@
 const request = require('supertest');
 const server = require('../server.js');
 
-//Note some of the tests take a 'token' - since the heroku deployment may have a different "secret" in environment variables will the tests fail?
+//Comment out server.listen in server.js before running tests - https://github.com/visionmedia/supertest/issues/568
+
+//Some of the tests take a 'token' - since the heroku deployment may have a different "secret" in environment variables will the tests fail?
+
+//Tokens expire in year 2033
 
 //Register
 
@@ -92,7 +96,7 @@ test('POST /login returns token if valid credentials for volunteer', () => {
 		.post('/login')
 		.send({ username: 'user1', password: 'user1', accountType: 'volunteer' })
 		.then((res) => {
-			expect(res.body).toHaveProperty('token');
+			expect(res.body).toHaveProperty('token')
 		});
 });
 
@@ -191,5 +195,122 @@ test('PUT /volunteer/username updates fileds provided', () => {
 		})
 		.then((res) => {
 			expect(res.body.message).toBe('Account updated!');
+		});
+});   
+
+////////////////////// Pickup requests
+
+test('POST /pickup creates pickup request if logged in as a business', () => {
+	return request(server)
+		.post('/pickup')
+		.set(
+			'Authorization',
+			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOjEsInVzZXJuYW1lIjoidXNlcjEiLCJhY2NvdW50VHlwZSI6ImJ1c2luZXNzIiwiaWF0IjoxNTk2MDgzMjk3LCJleHAiOjE5OTQ4MDY4NjF9.ea9ja57tFKH71H2PIJ9xds7FbtKzJfyZkqX3D4zI90I'
+		)
+		.send({
+			foodType: 'fruit',
+			amount: '900000 tons',
+			preferredPickupTime: '1 second from now',
+		})
+		.then((res) => {
+			expect(res.body.message).toBe('Pickup request created!');
+		});
+});
+
+test('POST /pickup fails if a required field is not provided', () => {
+	return request(server)
+		.post('/pickup')
+		.set(
+			'Authorization',
+			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOjEsInVzZXJuYW1lIjoidXNlcjEiLCJhY2NvdW50VHlwZSI6ImJ1c2luZXNzIiwiaWF0IjoxNTk2MDgzMjk3LCJleHAiOjE5OTQ4MDY4NjF9.ea9ja57tFKH71H2PIJ9xds7FbtKzJfyZkqX3D4zI90I'
+		)
+		.send({
+			amount: '900000 tons',
+			preferredPickupTime: '1 second from now',
+		})
+		.then((res) => {
+			expect(res.body.message).toBe(
+				'Provide a foodType, amount and preferredPickupTime.'
+			);
+		});
+});
+
+test('POST /pickup fails if logged in as a volunteer', () => {
+	return request(server)
+		.post('/pickup')
+		.set(
+			'Authorization',
+			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOjEsInVzZXJuYW1lIjoidXNlcjEiLCJhY2NvdW50VHlwZSI6InZvbHVudGVlciIsImlhdCI6MTU5NjA4MzI5NywiZXhwIjoxOTk0ODA2ODYxfQ.MYJR2AgVbCWS-qj70kOtFkqe-vI9zLCjXelZG06BiG0'
+		)
+		.send({
+			foodType: 'fruit',
+			amount: '900000 tons',
+			preferredPickupTime: '1 second from now',
+		})
+		.then((res) => {
+			expect(res.body.message).toBe(
+				'You do not have authorization to create a pickup request.'
+			);
+		});
+});
+
+test('GET /pickup returns all pickup requests', () => {
+	return request(server)
+		.get('/pickup')
+		.set(
+			'Authorization',
+			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOjEsInVzZXJuYW1lIjoidXNlcjEiLCJhY2NvdW50VHlwZSI6ImJ1c2luZXNzIiwiaWF0IjoxNTk2MDgzMjk3LCJleHAiOjE5OTQ4MDY4NjF9.ea9ja57tFKH71H2PIJ9xds7FbtKzJfyZkqX3D4zI90I'
+		)
+		.then((res) => {
+			expect(res.body[0]).toHaveProperty('id');
+		});
+});
+
+test('PUT /pickup/:id fails if not logged into the account that created the pickup request', () => {
+	return request(server)
+		.put('/pickup/58')
+		.set(
+			'Authorization',
+			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOjEsInVzZXJuYW1lIjoidXNlcjEiLCJhY2NvdW50VHlwZSI6ImJ1c2luZXNzIiwiaWF0IjoxNTk2MDgzMjk3LCJleHAiOjE5OTQ4MDY4NjF9.ea9ja57tFKH71H2PIJ9xds7FbtKzJfyZkqX3D4zI90I'
+		)
+		.send({
+			amount: '2 tons',
+		})
+		.then((res) => {
+			expect(res.body.message).toBe(
+				'You do not have authorization to update this pickup request.'
+			);
+		});
+});
+
+test('PUT /pickup/:id works if logged into the account that created the pickup request', () => {
+	return request(server)
+		.put('/pickup/2')
+		.set(
+			'Authorization',
+			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOjEsInVzZXJuYW1lIjoidXNlcjEiLCJhY2NvdW50VHlwZSI6ImJ1c2luZXNzIiwiaWF0IjoxNTk2MDgzMjk3LCJleHAiOjE5OTQ4MDY4NjF9.ea9ja57tFKH71H2PIJ9xds7FbtKzJfyZkqX3D4zI90I'
+		)
+		.send({
+			amount: '2 tons',
+		})
+		.then((res) => {
+			expect(res.body.message).toBe('Pickup request updated!');
+		});
+});
+
+test('PUT /pickup/:id setting status to Complete fails if not signed in as a volunteer that is assigned to the pickup request', () => {
+	return request(server)
+		.put('/pickup/2')
+		.set(
+			'Authorization',
+			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOjEsInVzZXJuYW1lIjoidXNlcjEiLCJhY2NvdW50VHlwZSI6InZvbHVudGVlciIsImlhdCI6MTU5NjA4MzI5NywiZXhwIjoxOTk0ODA2ODYxfQ.MYJR2AgVbCWS-qj70kOtFkqe-vI9zLCjXelZG06BiG0'
+		)
+		.send({
+			status: 'Complete',
+		})
+		.then((res) => {
+			expect(res.body.message).toBe(
+				'You do not have authorization to update this pickup request. The pickup request may not be assigned to you or is already complete.'
+			);
 		});
 });
